150c43d318849267f11152e1b6cbd8b8763a3b40
phil8192 <phil@parasec.net>
2018-10-03 18:57:14 +0000

    add satellite map + cleanup

--- app.js
+++ app.js
@@ -13,54 +13,11 @@ import {json as requestJson} from 'd3-request';
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
 
-// Source data GeoJSON
-//const DATA_URL = 'data/manchester.geojson';
-//const DATA_URL = 'data/cardiff.geojson';
 const DATA_URL = 'data/cardiff_segments.geojson';
-/*
- * colour scale.
- * "green index" colour for range [0, 1].
- *
- *
- * manchester green distribution:
- *
- *
- * quantile(x$V1, probs=seq(0,1,0.1))
- *     0%    10%    20%    30%    40%    50%    60%    70%    80%    90%   100% 
- * 0.0000 0.0236 0.0389 0.0544 0.0716 0.0916 0.1169 0.1503 0.2001 0.2862 0.8945 
- *
- *
- *
- * cardiff vegetation: 
- * > quantile(x$vegetation, seq(0,1,0.1))
- *       0%       10%       20%       30%       40%       50%       60%       70%
- *   0.0000000 0.0008101 0.0149082 0.0364843 0.0635820 0.0982200 0.1448196 0.2085075
- *       80%       90%      100%
- *   0.3044132 0.4721046 0.9999710
- *
- *
- *
- */
+
 var colorScale = (function() {
-  // green hues from http://colorbrewer2.org/
-
-	// fire (with blue for lowest values.)
-  //var hue = ['#555555', '#555555','#ffeda0','#fed976','#feb24c','#fd8d3c',
-  //           '#fc4e2a','#e31a1c','#bd0026', '#ff0000'];
-
-  // domain scaled to quantiles.
-  //var domain = [0.5,    0.2862, 0.2001, 0.1503, 0.1169, 
-  //              0.0916, 0.0716, 0.0544, 0.0389, 0.0236];
-  //var domain = [0.47, 0.3, 0.2, 0.15, 0.10, 0.05, 0.03, 0.1, 0.005, 0.001];
-  //var scale = chroma.scale(hue);//.domain(domain);
-
-
-
   var scale = chroma.scale(['white', 'yellow', 'orange', 'red', 'black']).domain([0.5, 0]);
-
-	return function(r) {
-    //var z = Math.log(r);
-    //return scale(z).alpha((1+r)*128).rgba();
+  return function(r) {
     return scale(r).alpha((1+r)*128).rgba(); 
   }
 })();
@@ -105,17 +62,10 @@ class Root extends Component {
 
   render() {
     const {viewport, data} = this.state;
-    // mapStyle="mapbox://styles/phil8192/cj5l7sfg126852rmagjutiw2e"
-    // mapStyle="mapbox://styles/mapbox/dark-v9"
-    // mapStyle="https://raw.githubusercontent.com/jingsam/mapbox-gl-styles/master/Camouflage.json"
-    // mapStyle="https://raw.githubusercontent.com/openmaptiles/dark-matter-gl-style/master/style.json"
-    // mapbox://styles/mapbox/traffic-night-v2
     return (
       <MapGL
         {...viewport}
-        mapStyle="https://raw.githubusercontent.com/jingsam/mapbox-gl-styles/master/Camouflage.json"
-        //mapStyle="mapbox://styles/phil8192/cjgmhugmy008l2rqj288139s7" // satellite
-        //mapStyle="mapbox://styles/phil8192/cjgmjp7nq00382rox3ibqo85c" // light  
+	mapStyle="mapbox://styles/mapbox/satellite-v9"
 	onViewportChange={this._onViewportChange.bind(this)}
         mapboxApiAccessToken={MAPBOX_TOKEN}>
         <DeckGLOverlay viewport={viewport}
--- deckgl-overlay.js
+++ deckgl-overlay.js
@@ -5,26 +5,11 @@ import DeckGL, {GeoJsonLayer} from 'deck.gl';
 export default class DeckGLOverlay extends Component {
   static get defaultViewport() {
     return {
-
-      /*
-      // manchester
-      latitude: 53.480793,
-      longitude: -2.242571,
-      */
-   
-      /*
-      // walsall
-      latitude: 52.578171,
-      longitude: -1.980528,
-      */
-
-      // cardiff
       latitude: 51.485078, 
       longitude: -3.176800,
-
-      zoom: 11,
+      zoom: 12,
       maxZoom: 20,
-      //pitch: 55,
+      pitch: 55, 
       bearing: 0
     };
   }
@@ -49,15 +34,6 @@ export default class DeckGLOverlay extends Component {
       fp64: true,
       getFillColor: f => colorScale(0.5*(f.properties.right.segments.vegetation+f.properties.left.segments.vegetation)),
       getRadius: f => Math.max(1, (0.5*(f.properties.right.segments.vegetation+f.properties.left.segments.vegetation))*40)
-      //getFillColor: f => colorScale(0.5*(f.properties.right.segments.building+f.properties.left.segments.building)),
-      //getRadius: f => Math.max(5, (0.5*(f.properties.right.segments.building+f.properties.left.segments.building))*20) 
-      //getFillColor: f => colorScale(Math.max(f.properties.right.segments.car,f.properties.left.segments.car)),
-      //getRadius: f => Math.max(10, (Math.max(f.properties.right.segments.car,f.properties.left.segments.car))*40)
-      //getFillColor: f => colorScale(Math.max(f.properties.right.segments.sky,f.properties.left.segments.sky)),
-      //getRadius: f => Math.max(10, (Math.max(f.properties.right.segments.sky,f.properties.left.segments.sky))*50)
-
-	    //pickable: true,
-      //onHover: info => console.log('Hovered:', info)
     });
 
     return (
